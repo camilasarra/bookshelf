a323706d0e9ae3c072a37e1dadc922b02402fa07
camilasarra <camilasarra@gmail.com>
2024-03-26 14:51:15 -0300

    new-components

--- front/front/src/App.jsx
+++ front/front/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Public from './components/Public'
-import Login from './components/Login'
+import Login from './components/features/Login'
 import DashLayout from './components/DashLayout'
+import Welcome from './components/features/Welcome'
+import ReviewsList from './components/features/reviews/ReviewsList'
+import UsersList from './components/features/users/UsersList'
 
 export default function App() {
   return (
@@ -13,6 +16,16 @@ export default function App() {
         <Route path='login' element={<Login />} />
 
         <Route path='dash' element={<DashLayout />}>
+
+          <Route index element={<Welcome />} />
+
+          <Route path='reviews'>
+            <Route index element={<ReviewsList />} />   
+          </Route>
+
+          <Route path='users'>
+            <Route index element={<UsersList />} />   
+          </Route>
           
         </Route>
 
