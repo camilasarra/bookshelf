04661b567e1b4e0a327753f0693c14437a05b7a8
camilasarra <camilasarra@gmail.com>
2024-04-10 12:28:38 -0300

    redux

--- front/front/src/main.jsx
+++ front/front/src/main.jsx
@@ -4,13 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import { store } from './redux/store.js'
+import { Provider } from "react-redux"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <Routes>
       <Route path='/*' element={<App /> } />
     </Routes>
     </BrowserRouter>
   
+    </Provider>
   </React.StrictMode>,
 )
